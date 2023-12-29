7ea65a0f4883b917f7724ac4b5fdf1a27409af03
Braian Mena <braianmena@gmail.com>
2023-12-29 19:36:49 -0300

    Agregado del template/estructura de la pagina.

---
+++ js/utils/cartBadgeHandler.js
@@ -0,0 +1,9 @@
+import { getLoggedUser } from "../services/getLoggedUser.js";
+
+/**
+ * @returns {number} Renderiza la cantidad de productos en el carrito. 
+ */
+//incrementa o decrementa el icono encima del carrito
+export const cartBadgeHandler =()=>{
+
+}
