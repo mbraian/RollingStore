7f03ad7c40e92b5fe31dec5e4e6a0531b871167a
Braian Mena <braianmena@gmail.com>
2023-12-29 21:30:13 -0300

    Implementacion de servicio createUser. Falta probar por falta de implementacion de funciones de las cuales depende.

--- js/services/createUser.js
+++ js/services/createUser.js
@@ -1,5 +1,8 @@
 import { User } from "../classes/user.class.js";
 import { getUsers } from "./getUsers.js";
+import { validateEmail } from "../validators/validateEmail.js";
+import { validateExistingEmail } from "../validators/validateExistingEmail.js";
+import { validateSignUpPassword } from "../validators/validateSignUpPassword.js";
 /**
  *
  * @param {string} email Recibe un email valido
@@ -8,6 +11,22 @@ import { getUsers } from "./getUsers.js";
  */
 
 export const createUser = ({ email, password }) => {
-  const users = getUsers()
-  localStorage.setItem('users', JSON.stringify(email, password));
+  const users = getUsers();
+
+  if(validateEmail(email)){
+    if(validateExistingEmail(email) && validateSignUpPassword(password)){
+      
+      if(users.some(u => u.email === email)){return};
+
+      const nuevoUser = new User(email, password, "user");
+      users.push(nuevoUser);
+      localStorage.setItem('users', JSON.stringify(users));
+      console.log(`Usuario: ${email}\nPassword:${password} \nLogueado exitosamente.`);
+      
+    }else{
+      console.warn("El email ya existe o la contraseña es incorrecta.");
+    }
+  }else{
+    console.warn("Email inválido.")
+  }
 };
