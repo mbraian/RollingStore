1ee9a9154a70f610b8bb8add8ec27b718431148a
Braian Mena <braianmena@gmail.com>
2024-01-02 17:22:29 -0300

    Implementación 'getUsers'.

--- js/services/getUsers.js
+++ js/services/getUsers.js
@@ -5,5 +5,23 @@
  */
 
 export const getUsers = () =>{
-  localStorage.getItem(JSON.parse('users'));
+  const usuariosCadena = localStorage.getItem('users');
+
+  if(usuarios){
+    try{
+      const usuariosArray = JSON.parse(usuariosCadena);
+
+      if(Array.isArray(usuariosArray)){
+        console.log(usuariosArray,"<---getUsers-Exito")
+        return usuariosArray;
+      }else{
+        console.warn("El contenido almacenado en localStorage no es un array.");
+        return [];
+      }
+    }catch{
+      console.warn("Error al parsear la cadena JSON:", error);
+    }
+  }else{
+    return [];
+  }
 }
