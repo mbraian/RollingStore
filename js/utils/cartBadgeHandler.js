aa24e105a3c9840d2f1afa314cd0af090a049172
Braian Mena <braianmena@gmail.com>
2024-01-06 18:50:20 -0300

    Implementacion de addProductToCard: cartBadgeHandler

--- js/utils/cartBadgeHandler.js
+++ js/utils/cartBadgeHandler.js
@@ -5,5 +5,18 @@ import { getLoggedUser } from "../services/getLoggedUser.js";
  */
 //incrementa o decrementa el icono encima del carrito
 export const cartBadgeHandler =()=>{
+    const usuario = getLoggedUser();
+    const cartBadge = document.getElementById('cart-badge');
+    let carrito = JSON.parse(localStorage.getItem(usuario?.id));
+    let cantCadaProduct = carrito?.map(p => p.quanity);
+    let valorInicial = 0;
 
+    let totalProducts = cantCadaProduct?.reduce((prev, actual)=>{prev+actual},valorInicial);
+
+    if((!carrito || totalProducts == 0) && usuario){ //Si el carrito está vacío o la suma de productos es 0, y hay un usuario logueado, se oculta el "badge" (classList.add("d-none")).
+        cartBadge.classList.add("d-none");
+    }else if(usuario){ // Si hay un usuario logueado y el carrito no está vacío, muestra el número de productos en el "badge" y lo muestra (classList.remove("d-none")).
+        cartBadge.innerText = totalProducts;
+        cartBadge.classList.remove('d-none');
+    }
 }
