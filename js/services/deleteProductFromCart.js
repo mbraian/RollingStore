315130462240693cbc189f851591470910c39666
Braian Mena <braianmena@gmail.com>
2024-01-07 17:22:00 -0300

    Implementacion de CartCard: deleteProductFromCart

--- js/services/deleteProductFromCart.js
+++ js/services/deleteProductFromCart.js
@@ -2,21 +2,23 @@ import { getProducts } from "./getProducts.js";
 import { getCartProducts } from "./getCartProducts.js";
 import { renderCartBody } from "../cart.init.js";
 import { cartBadgeHandler } from "../utils/cartBadgeHandler.js";
-
+import { setCartProducts } from "./setCartProducts.js";
+import { getLoggedUser } from "./getLoggedUser.js";
 /**
  * 
  * @param {*} id Recibe el id del producto a eliminar
  * @returns {} Elimina el producto del carrito
  */
 export const deleteProductFromCart = (id) => {
-    const cartProducts = getCartProducts()
-    const productFoundInCart = cartProducts.find(product => product.id == id)
-    const productFoundInCartIndex = cartProducts.findIndex(product => product.id == id)
+    const usuarioLogueado = getLoggedUser();
+    const cartProducts = getCartProducts();
+    const productFoundInCart = cartProducts.find(product => product.id == id);
+    const productFoundInCartIndex = cartProducts.findIndex(product => product.id == id);
 
 
     if (productFoundInCart ) {
-        cartProducts.splice(productFoundInCartIndex,1)
-        localStorage.setItem("cart", JSON.stringify(cartProducts));
+        cartProducts.splice(productFoundInCartIndex,1);
+        setCartProducts(usuarioLogueado.id, cartProducts);
     }
 
     cartBadgeHandler()
